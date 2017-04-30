b40704ea783a2a8b6ecac3a6a2fe673ae8635d69
kongdejian <dejian.kdj@autonavi.com>
2017-05-01 06:09:21 +0800

    样式修改

--- modules/home/js/index.js
+++ modules/home/js/index.js
@@ -5,7 +5,7 @@ var Index = function () {
     this.init.apply(this, arguments);
 };
 $.extend(Index.prototype, {
-    iframe:$('.main-iframe'),
+    currentModule:null,
     init: function () {
         var _this = this;
         _this.setMenu();
@@ -21,22 +21,31 @@ $.extend(Index.prototype, {
                     var pageData = data.pageList[i];
                     eleStr +='<li';
                     if(i==0){
-                        eleStr +=' class="tag-type"';
-                        _this.iframe.attr('src',pageData.url).data('url',pageData.url);
+                        eleStr +=' class="current-nav"';
                     }
                     eleStr +='><a href="javascript:;" data-url="'+pageData.url+'">'+pageData.title+'</a></li>';
                 }
                 $('.navmenu').html(eleStr);
+                _this.getData();
             }
         });
         $('.navmenu').on('click','a',function () {
             var url = $(this).data('url');
             $(this).parents('li').addClass('current-nav').siblings('.current-nav').removeClass('current-nav');
-            var iframeUrl = _this.iframe.data('url');
-            url!=iframeUrl&&_this.iframe.attr('src',url).data('url',url);
+            _this.getData();
         });
     },
     bindEvent:function () {
+    },
+    getData:function () {
+        var url = $('.navmenu .current-nav a').data('url');
+        if(this.currentModule==url)return;
+        this.currentModule=url;
+        $.getJSON(this.currentModule+'./modules/config.json', function(res){
+            if(res.code==1){
+                console.log(res);
+            }
+        });
     }
 });
 new Index();
