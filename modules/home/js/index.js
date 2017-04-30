3fa4f20b5e5cf206aca34bf0d4f928a91c121750
kongdejian <dejian.kdj@autonavi.com>
2017-05-01 07:31:16 +0800

    三级目录

--- modules/home/js/index.js
+++ modules/home/js/index.js
@@ -6,6 +6,8 @@ var Index = function () {
 };
 $.extend(Index.prototype, {
     currentModule:null,
+    currentPage:0,
+    pageData:null,
     init: function () {
         var _this = this;
         _this.setMenu();
@@ -38,14 +40,29 @@ $.extend(Index.prototype, {
     bindEvent:function () {
     },
     getData:function () {
+        var _this =this;
         var url = $('.navmenu .current-nav a').data('url');
         if(this.currentModule==url)return;
-        this.currentModule=url;
+        _this.currentModule=url;
         $.getJSON(this.currentModule+'./modules/config.json', function(res){
             if(res.code==1){
                 console.log(res);
+                _this.currentPage=0;
+                _this.pageData=res.data;
+                _this.showPage();
             }
         });
+    },
+    showPage:function () {
+        var _this =this;
+        var eleStr = '';
+        for(var i=0;i<20;i++){
+            var index = _this.currentPage*20+i;
+            if(index>=_this.pageData.pageList.length)break;
+            var pageData = _this.pageData.pageList[index];
+            eleStr+='<li class="news" data-url="'+this.currentModule+pageData.url+'"><div class="newsImg"><span style="background-image: url('+this.currentModule+pageData.url+'imgs/banner.jpg);"></span></div><div class="newsDesc"><h2 class="newstitle">'+pageData.title+'</h2><p class="newsintro">'+pageData.desc+'</p></div></li>';
+        }
+        $('.newsList').html(eleStr);
     }
 });
 new Index();
